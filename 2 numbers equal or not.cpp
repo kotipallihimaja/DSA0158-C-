39077f99f13780fa1e4b2371d1383a24437952f3
kotipallihimaja <141385009+kotipallihimaja@users.noreply.github.com>
2024-07-22 09:49:37 +0530

    Add files via upload

---
+++ 2 numbers equal or not.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num1,num2;
+	cout<<"enter the first number:";
+	cin>>num1;
+	cout<<"enter the second number:";
+	cin>>num2;
+	if(num1==num2)
+	{
+		cout <<"the numbers are equal."<<endl;
+	}
+		else
+		{
+			cout << "The numbers are not equal." << endl;
+		}
+	
+	return 0;
+}
