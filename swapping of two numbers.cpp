f0da360bb54cbee2d0348d526654e696d6f5c100
kotipallihimaja <141385009+kotipallihimaja@users.noreply.github.com>
2024-07-22 15:34:42 +0530

    Add files via upload

---
+++ swapping of two numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int a, b, temp;
+    cout << "Enter first number (a): ";
+    cin >> a;
+    cout << "Enter second number (b): ";
+    cin >> b;
+    cout << "Before swapping: a = " << a << ", b = " << b << endl;
+    temp = a;
+    a = b;
+    b = temp;
+    cout << "After swapping: a = " << a << ", b = " << b << endl;
+    return 0;
+}
