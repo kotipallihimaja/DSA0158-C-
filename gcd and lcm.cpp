827fa7c85c7ac2f6785e8a8c30b97c1d95bea1ff
kotipallihimaja <141385009+kotipallihimaja@users.noreply.github.com>
2024-07-22 15:22:04 +0530

    Add files via upload

---
+++ gcd and lcm.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+int lcm(int a, int b) {
+    return (a * b) / gcd(a, b);
+}
+int main() {
+    int num1, num2;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    int gcdResult = gcd(num1, num2);
+    int lcmResult = lcm(num1, num2);
+    cout << "GCD of " << num1 << " and " << num2 << " is: " << gcdResult << endl;
+    cout << "LCM of " << num1 << " and " << num2 << " is: " << lcmResult << endl;
+    
+    return 0;
+}
