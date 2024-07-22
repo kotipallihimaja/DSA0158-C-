b63729daeb5486d8ff04c264114b07c1bfceaf13
kotipallihimaja <141385009+kotipallihimaja@users.noreply.github.com>
2024-07-22 15:19:32 +0530

    Add files via upload

---
+++ even or odd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+  int a;
+  cout << "Enter the number: "; 
+  cin >> a;
+  if (a % 2 == 0) 
+  {
+    cout << "The given number is EVEN" << endl;
+  } else 
+  {
+    cout<<"the given muber is ODD"<<endl;
+}
+return 0;
+}
